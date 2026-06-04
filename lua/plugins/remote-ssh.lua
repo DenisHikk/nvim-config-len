0bdde232e48644ab2845224d259b4355c2207f76
Denis <qartoonnetwork@gmail.com>
2026-06-04 22:51:09 +0500

    build(plugin): change remote-ssh

--- lua/plugins/remote-ssh.lua
+++ lua/plugins/remote-ssh.lua
@@ -1,6 +1,13 @@
 return {
 	{
-		"uhs-robert/sshfs.nvim",
-		opts = {},
+		"amitds1997/remote-nvim.nvim",
+		build = "cargo build --release",
+		version = "*",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		config = true,
 	}
 }
